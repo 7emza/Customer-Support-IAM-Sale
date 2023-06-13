7ab30e6059d27f34c843b5f9ee7d60975615bac1
Hamza Driouch <hdri@concepment.com>
2023-06-13 19:44:29 +0100

    update seedres'

--- database/seeders/Issues.php
+++ database/seeders/Issues.php
@@ -15,19 +15,20 @@ class Issues extends Seeder
      */
     public function run()
     {
- 
+
         $statuses = ['Submitted', 'In Progress', 'Resolved', 'Closed'];
- 
- 
+        $loremIpsum = "Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+        $words = explode(" ", $loremIpsum);
+       
+
         $users = User::role('customer')->get();
- 
+
         // Generate 20 issues
         for ($i = 0; $i < 20; $i++) {
             $status = $statuses[array_rand($statuses)];
             $user = $users->random();
-            $admins = 
-            $subject = 'Issue ' . ($i + 1);
-            $details = 'Details for issue ' . ($i + 1);
+            $subject = ucfirst($words[array_rand($words)].' Issue '.$i) ;
+            $details = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas eaque porro reprehenderit facere vitae itaque cum, labore odit magni officiis eum. Officiis eius nobis iusto adipisci minima sit obcaecati eveniet.' . ($i + 1);
 
             Issue::create([
                 'user_id' => $user->id,
